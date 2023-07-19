4e760d983b1299f1313390617767d34df7923c1e
Evonic4 <46780974+Evonic4@users.noreply.github.com>
2023-07-19 19:59:58 +0300

    rex

--- rex.sh
+++ rex.sh
@@ -1,60 +1,68 @@
 #!/bin/bash
 
-home_trbot=/usr/share/abot2/
-fhome=$home_trbot
-
+fhome=/usr/share/abot2/
+bui=$(sed -n 11"p" $fhome"settings.conf" | tr -d '\r')
+log=$fhome"rex_log.txt"
 f_text=$1
-echo $f_text
+echo > $log
+
+function logger()
+{
+local date1=`date '+ %Y-%m-%d %H:%M:%S'`
+echo $date1" rex_"$bui": "$1 >> $log
+}
 
 
+logger "start "$f_text
 
 dl=$(wc -m $f_text | awk '{ print $1 }')
-echo "dl="$dl
+logger "dl="$dl
+
 if [ "$dl" -gt "4000" ]; then
 	sv=$(echo "$dl/4000" | bc)
 	sv=$((sv+1))
-	echo "sv="$sv
+	logger "sv="$sv
 	i1=0
 	
 	for (( i=1;i<=$sv;i++)); do
-		[ "$i1" -gt "$str_col" ] && break
-		echo "--------i="$i"------"
+		logger "--------i="$i"------"
+		[ "$i" -gt "$sv" ] && logger "i="$i" > sv="$sv" break" && break
 		str_col=$(grep -cv "^----------" $f_text)
-		echo "str_col="$str_col
+		logger "str_col="$str_col
 		
 		again2="yes"
 		i1=0
 		odl=0
 		while [ "$again2" = "yes" ]
 			do
-			echo "odl="$odl
+			logger "odl="$odl
 			i1=$((i1+1))
-			echo "i1="$i1
+			logger "i1="$i1
 			test=$(sed -n $i1"p" $f_text | tr -d '\r')
 			len1=${#test}
 			len1=$((len1+1))
-			echo "len1="$len1
+			logger "len1="$len1
 			od2=$((odl+len1))
-			echo "od2="$od2
+			logger "od2="$od2
 			if [ "$od2" -gt "4000" ]; then
-				echo "start rez"
+				logger "start rez"
 				i1=$((i1-1))
-				echo "new i1="$i1
+				logger "new i1="$i1
 				head -n $i1 $f_text > $fhome"rez"$i".txt"
 				tcol=$((str_col-i1))
-				echo "tcol="$tcol
+				logger "tcol="$tcol
 				tail -n $tcol $f_text > $fhome"rez_tmp.txt"
-				echo "rez-------------------->"
+				logger "rez-------------------->rez"$i".txt"
 				cp -f $fhome"rez_tmp.txt" $f_text
 				
 				#read var1
 				again2="no"
 			else
-				echo "not rez"
+				logger "not rez"
 				odl=$od2
 			fi
 			if [ "$i1" -gt "$str_col" ]; then
-				echo "end rez"$i1
+				logger "end rez i1="$i1
 				cp -f $f_text $fhome"rez"$i".txt"
 				again2="no"
 				break
