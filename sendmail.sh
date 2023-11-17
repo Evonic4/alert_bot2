6e7106969d0903340126b20f61ea64f4ffd729d8
Evonic4 <46780974+Evonic4@users.noreply.github.com>
2023-11-17 14:00:38 +0300

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -3,16 +3,25 @@ export PATH="$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
 
 fhome=/usr/share/abot2/
 fmail=$fhome"mail.txt"
-
+bui=$(sed -n 11"p" $fhome"sett.conf" | tr -d '\r')
 smtp_user=$(sed -n 38"p" $fhome"sett.conf" | tr -d '\r')
 to_mail=$(sed -n 41"p" $fhome"sett.conf" | tr -d '\r')
 MSUBJ=$(sed -n "1p" $fmail | tr -d '\r')
 MBODY=$(sed -n "2p" $fmail | tr -d '\r')
 
+
+function logger()
+{
+local date1=$(date '+ %Y-%m-%d %H:%M:%S')
+echo $date1" sendmail_"$bui": "$1
+}
+
+
+
 if ! [ "$to_mail" == "" ]; then
 for MADDR in $(echo $to_mail | tr " " "\n")
 do
-echo "send mail to "$MADDR
+logger "send mail to "$MADDR
 
 IFS=$'\x10'
 printf '%s\n' "Subject: "$MSUBJ \
@@ -24,5 +33,5 @@ unset IFS
 
 done
 else
-	echo "to_mail is NULL"
+	logger "to_mail is NULL"
 fi
