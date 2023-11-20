b74674f39b3ca5d56a5ceca991a5128360183be3
Evonic4 <46780974+Evonic4@users.noreply.github.com>
2023-11-21 00:33:46 +0300

    0.65

--- abot3.sh
+++ abot3.sh
@@ -327,14 +327,23 @@ smail()
 if ! [ "$smtp_hostname" == "" ] && ! [ "$smtp_sport" == "" ] && ! [ "$smtp_user" == "" ] && ! [ "$smtp_pass" == "" ]; then
 logger "smail"
 cp -f $fhome"sendmail_tmp.sh" $fhome"sendmail.sh"
-echo "su monitoring -c 'cd; echo \""$MBODY"\" | mail -s \""$MSUBJ"\" \$MADDR' -s /bin/bash" >> $fhome"sendmail.sh"
-echo "done" >> $fhome"sendmail.sh" 
-echo "else" >> $fhome"sendmail.sh"
-echo "	echo \"to_mail is NULL\"" >> $fhome"sendmail.sh"
-echo "fi" >> $fhome"sendmail.sh"
+
+to_mail=$(sed -n 41"p" $fhome"sett.conf" | tr -d '\r')
+
+if ! [ "$to_mail" == "" ]; then
+	for MADDR in $(echo $to_mail | tr " " "\n")
+	do
+	logger "smail send mail to "$MADDR
+	
+	echo "su monitoring -c 'cd; echo \""$MBODY"\" | mail -s \""$MSUBJ"\" "$MADDR"' -s /bin/bash" >> $fhome"sendmail.sh"
+	
+	done
+else
+	logger "smail to_mail is NULL"
+fi
+
 chmod +rx $fhome"sendmail.sh"
 $fhome"sendmail.sh"
-fi
 }
 
 
--- sendmail_tmp.sh
+++ sendmail_tmp.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 export PATH="$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
 
-fhome=/usr/share/abot2/
-to_mail=$(sed -n 41"p" $fhome"sett.conf" | tr -d '\r')
-
-if ! [ "$to_mail" == "" ]; then
-for MADDR in $(echo $to_mail | tr " " "\n")
-do
-echo "send mail to "$MADDR
